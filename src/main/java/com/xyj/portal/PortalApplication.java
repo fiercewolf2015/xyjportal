d51a24411e2f242c8f5499eb4676b9197319fb5c
lihk <lihk@tjnetsec.com>
2018-12-04 14:30:08 +0800

    Change to tomcat start

--- src/main/java/com/xyj/portal/PortalApplication.java
+++ src/main/java/com/xyj/portal/PortalApplication.java
@@ -1,12 +1,14 @@
 package com.xyj.portal;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PortalApplication {
+public class PortalApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PortalApplication.class, args);
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PortalApplication.class);
 	}
 }
